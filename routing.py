5085d2cf2b9e128302f22f244b8af0aa23aecf50
Dinith1 <wannigamadinith@gmail.com>
2019-05-27 23:10:31 +1200

    updated files from canvas

--- routing.py
+++ routing.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8 -*-
 
-def dijkstra_predecessor_and_distance(graph, source, weight='weight'):
-    """
-    Least-cost paths via Dijkstra's algorithm.
-    """
-	pass  # TODO: Please work from Lab 1 code
-
-
-def predecessor_to_forwarding(predecessor, source):
+def forwarding(predecessor, source):
     """ 
     Compute a forwarding table from a predecessor list. 
     """
